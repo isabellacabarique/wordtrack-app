2bda511cfb3c56e4f7918c20d09a31b18a8104ac
isabellacabarique <isabellacabarique@gmail.com>
2026-07-23 16:27:20 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+// Service worker de WordTrack: permite que la app sea instalable
+// y que abra aunque el celular pierda la conexión por un momento.
+
+const CACHE_NAME = 'wordtrack-cache-v1';
+const APP_SHELL = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Estrategia: intenta la red primero (para traer cambios nuevos),
+// si falla (sin conexión) usa lo que haya guardado en caché.
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
